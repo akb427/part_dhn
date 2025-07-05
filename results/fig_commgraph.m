9e196d2f17a279ece8abc63c04bda8ab8c642e68
akb427 <akb427@gmail.com>
2025-07-05 16:27:26 -0400

    remove redundancies

--- results/fig_commgraph.m
+++ results/fig_commgraph.m
@@ -1,68 +1,10 @@
-function [Gcomm] = fig_commgraph(G,elems,num,params_plot)
+function fig_commgraph(elems,num,se,params_plot)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
 %% Create Communication Graphs
 
-Gcomm = cell(1,3);
-adj_comm = cell(1,3);
-% mass flow
-adj_comm{1} = false(num.edge+2);
-for idx_edge = 1:num.edge
-   out_node = G.Edges.EndNodes(idx_edge,2);
-    if out_node==elems.term
-        adj_comm{1}(idx_edge,num.edge+2) = 1;
-    else
-        if ismember(idx_edge,elems.hot)
-            adj_comm{1}(outedges(G,out_node),idx_edge) = 1;
-        else
-            adj_comm{1}(idx_edge,outedges(G,out_node)) = 1;
-        end
-    end
-end
-adj_comm{1}(num.edge+1,outedges(G,elems.root)) = 1; % outedges of root
-Gcomm{1} = digraph(adj_comm{1});
-
-% Pressure
-adj_comm{2} = false(num.edge+2);
-for idx_edge = 1:num.edge
-    out_node = G.Edges.EndNodes(idx_edge,2);
-    if out_node==elems.term  % connected to reference pressure
-        adj_comm{2}(num.edge+2,idx_edge) = 1;
-    else
-        if ismember(idx_edge,elems.hot)
-            adj_comm{2}(idx_edge,outedges(G,out_node)) = 1;
-        else
-            adj_comm{2}(outedges(G,out_node),idx_edge) = 1;
-        end
-    end
-end
-adj_comm{2}(num.edge+1,outedges(G,elems.root)) = 1;
-Gcomm{2} = digraph(adj_comm{2});
-
-% Temperature
-adj_comm{3} = false(num.edge+2);
-for idx_edge = 1:num.edge
-    in_node = G.Edges.EndNodes(idx_edge,1);
-    if in_node == elems.root
-        adj_comm{3}(num.edge+1,idx_edge) = 1; % outedges of root
-    else
-        adj_comm{3}(inedges(G,in_node),idx_edge) = 1;
-    end
-    if G.Edges.EndNodes(idx_edge,2)==elems.term
-        adj_comm{3}(idx_edge,num.edge+2) = 1;
-    end
-end
-Gcomm{3} = digraph(adj_comm{3});
-
-for idx_g = 1:3
-    Gcomm{idx_g}.Edges.("Idx") = idx_g*ones(numedges(Gcomm{idx_g}),1);
-end
-% Combine
-G_all = Gcomm{1};
-G_all = addedge(G_all,Gcomm{2}.Edges);
-G_all = addedge(G_all,Gcomm{3}.Edges);
-
+[Gconv,Gcomm,~] = comm_graph(elems,num,se);
 
 %% Properties
 
@@ -94,7 +36,7 @@ L.FontSize = params_plot.ft;
 set(gca,'xtick',[],'ytick',[])
 
 % Plot
-h = plot(G_all,'XData',x_coord, 'YData',y_coord);
+h = plot(Gconv,'XData',x_coord, 'YData',y_coord);
 h.NodeLabel = node_labels;
 h.Interpreter = 'latex';
 h.NodeFontSize = params_plot.ft;
@@ -108,7 +50,7 @@ h.ArrowSize = 9;
 h.EdgeAlpha = 1;
 
 for idx_g = 1:3
-    highlight(h,'Edges',G_all.Edges.Idx==idx_g,'LineWidth',1.5,'EdgeColor',params_plot.comm.clr(idx_g,:),'LineStyle', params_plot.comm.lnsty(idx_g));
+    highlight(h,'Edges',Gconv.Edges.Idx==idx_g,'LineWidth',1.5,'EdgeColor',params_plot.comm.clr(idx_g,:),'LineStyle', params_plot.comm.lnsty(idx_g));
 end
 
 % Manually add node labels
