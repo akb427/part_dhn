0622816b38b2390c2c28aeb0ec6ee835c18ba089
akb427 <akb427@gmail.com>
2025-07-05 12:34:13 -0400

    function description

--- regression/get_commlink.m
+++ regression/get_commlink.m
@@ -1,5 +1,22 @@
-function comm_link = get_commlink(part,I)
-    ne = size(I,2);
+function [comm_link] = get_commlink(part,I)
+%GET_COMMLINK  Communication links between partitioned elements.
+%
+%   [comm_link] = GET_COMMLINK(part,I)
+%
+%   DESCRIPTION:
+%   Get information about what elements have knowledge of others preferred
+%   behaviors based on the system partitioning.
+%
+%   INPUTS:
+%       part    - Vector of partition being considered
+%       I       - Graph incidence matrix
+%
+%   OUTPUTS:
+%       comm_link - Binary vector of if communication exists between
+%       elements
+
+%% Find links 
+    ne = size(I,2); % number of elements
     comm_link = false(1,ne);
     for idx_e = 1:ne
         pi = part(I(:,idx_e));
